81548e01981e342cd92022016d669ba7557ef0f7
Marek Jarczewski <marek.jarczewski81@gmail.com>
2023-08-15 22:11:27 +0200

    Created first Author model & make migrations

--- src/authors/models.py
+++ src/authors/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+    
+    def _str_(self):
+        return f"Author: {self.name}"
