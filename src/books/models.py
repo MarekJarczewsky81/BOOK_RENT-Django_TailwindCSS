f1fa84a34a6326f13aa2cb020e57275fd6f47bf8
Marek Jarczewski <marek.jarczewski81@gmail.com>
2023-08-15 23:53:48 +0200

    Eddited models in books

--- src/books/models.py
+++ src/books/models.py
@@ -2,10 +2,14 @@ from django.db import models
 from publishers.models import Publisher
 from authors.models import Author
 from django.utils.text import slugify
-
+import uuid
 # Create your models here.
+"""
+Now:
+book_id / isbn - individual book identifier (uuid)
 
 
+"""
 class BookTitle(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(blank=True)
@@ -13,14 +17,12 @@ class BookTitle(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-
+    
     def __str__(self):
         return f"Book position: {self.title}"
     
-    
     def save(self, *args, **kwargs):
         if not self.slug:
-            # generate the slug and import Slugify up
             self.slug = slugify(self.title)
         super().save(*args, **kwargs)
 
@@ -34,3 +36,8 @@ class Book(models.Model):
 
     def __str__(self):
         return str(self.title)
+
+    def save(self, *args, **kwargs):
+        if not self.book_id:
+            self.book_id = str(uuid.uuid4()).replace('-', '')[:24].lower()
+        super().save(*args, **kwargs)
