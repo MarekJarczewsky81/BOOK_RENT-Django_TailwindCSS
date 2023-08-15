5c83e0066408203376a31fe005d283e453a04ddf
Marek Jarczewski <marek.jarczewski81@gmail.com>
2023-08-16 01:18:45 +0200

    Added QrCode to Books

--- src/authors/models.py
+++ src/authors/models.py
@@ -2,12 +2,13 @@ from django.db import models
 
 # Create your models here.
 
+
 class Author(models.Model):
     """
     Book author class
     Managed only in django admin
     """
     name = models.CharField(max_length=200)
-    
-    def _str_(self):
+
+    def __str__(self):
         return f"Author: {self.name}"
--- src/books/models.py
+++ src/books/models.py
@@ -3,13 +3,14 @@ from publishers.models import Publisher
 from authors.models import Author
 from django.utils.text import slugify
 import uuid
-# Create your models here.
-"""
-Now:
-book_id / isbn - individual book identifier (uuid)
+
+# imports for qrcode generation
+import qrcode
+from io import BytesIO
+from django.core.files import File
+from PIL import Image
 
 
-"""
 class BookTitle(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(blank=True)
@@ -17,10 +18,10 @@ class BookTitle(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    
+
     def __str__(self):
         return f"Book position: {self.title}"
-    
+
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.title)
@@ -40,4 +41,15 @@ class Book(models.Model):
     def save(self, *args, **kwargs):
         if not self.book_id:
             self.book_id = str(uuid.uuid4()).replace('-', '')[:24].lower()
+
+            # generate qr_code
+            qrcode_img = qrcode.make(self.book_id)
+            canvas = Image.new('RGB', (qrcode_img.pixel_size, qrcode_img.pixel_size), 'white')
+            canvas.paste(qrcode_img)
+            fname = f'qr_code-{self.title}.png'
+            buffer = BytesIO()
+            canvas.save(buffer, 'PNG')
+            self.qr_code.save(fname, File(buffer), save=False)
+            canvas.close()
+
         super().save(*args, **kwargs)
