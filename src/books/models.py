8bac2fd15127dcfe0612c1da96f2536e0e4513a3
Marek Jarczewski <marek.jarczewski81@gmail.com>
2023-08-15 23:19:37 +0200

    Added classes to models

--- src/books/models.py
+++ src/books/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from publishers.models import Publisher
+from authors.models import Author
 
 # Create your models here.
+
+
+class BookTitle(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(blank=True)
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Book position: {self.title}"
+
+
+class Book(models.Model):
+    title = models.ForeignKey(BookTitle, on_delete=models.CASCADE)
+    book_id = models.CharField(max_length=24, blank=True)
+    # qr_code
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.title)
