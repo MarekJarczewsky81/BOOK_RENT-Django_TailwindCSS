3f387ce23ba3f02c586414ab951cadc4485b6881
Marek Jarczewski <marek.jarczewski81@gmail.com>
2023-08-16 15:52:52 +0200

    Created first home_view

--- src/core/urls.py
+++ src/core/urls.py
@@ -20,9 +20,12 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from .views import home_view
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_view),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
