e8ce8c58e0693019c36f5bb3fbc01d2bbc128875
Marek Jarczewski <marek.jarczewski81@gmail.com>
2023-08-16 16:29:07 +0200

    Added main.html

--- src/core/views.py
+++ src/core/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 
 def home_view(request):
     value = 'hello world 5'
-    return render(request, 'main.html', {'key_in_the_template': value})
+    return render(request, 'main.html', {'value': value})
