8f0504026e670d6cefe047e0599bd8d25a3f05b2
Marek Jarczewski <marek.jarczewski81@gmail.com>
2023-08-16 15:26:42 +0200

    Added Rental model

--- src/rentals/models.py
+++ src/rentals/models.py
@@ -1,3 +1,33 @@
 from django.db import models
-
+from books.models import Book
+from customers.models import Customer
+from datetime import timedelta
 # Create your models here.
+
+STATUS_CHOICES = (
+    ('#0', 'rented'),
+    ('#1', 'returned'),
+    ('#2', 'lost'),
+    ('#3', 'delayed'),
+)
+
+
+class Rental(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    status = models.CharField(max_length=2, choices=STATUS_CHOICES)
+    rent_start_date = models.DateField(help_text='when the book was rented')
+    rent_end_date = models.DateField(help_text='deadline', blank=True)
+    return_date = models.DateField(
+        blank=True, null=True, help_text='actual return date')
+    is_closed = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.book.book_id} rented by {self.customer.username}'
+
+    def save(self, *args, **kwargs):
+        if not self.rent_end_date:
+            self.rent_end_date = self.rent_start_date + timedelta(days=14)
+        super().save(*args, **kwargs)
