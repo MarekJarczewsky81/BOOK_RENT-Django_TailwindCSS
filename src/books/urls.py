edfcfd233362346bb897cb24e8dbb9bbfabcefbc
Marek Jarczewski <marek.jarczewski81@gmail.com>
2023-09-23 16:35:38 +0200

    App views and URLs, add a detail view

--- src/books/urls.py
+++ src/books/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import book_title_list_view
+from .views import book_title_list_view, book_title_detail_view
 
 app_name = 'books'
 
 urlpatterns = [
 
-    path('', book_title_list_view),
-
+    path('', book_title_list_view, name='main'),
+    path('<pk>/', book_title_detail_view, name='detail'),
 ]
--- src/books/views.py
+++ src/books/views.py
@@ -5,4 +5,10 @@ from .models import BookTitle
 
 def book_title_list_view(request):
     qs = BookTitle.objects.all()
-    return render(request, 'books/main.html', {'qs':qs})
+    return render(request, 'books/main.html', {'qs': qs})
+
+
+def book_title_detail_view(request, **kwargs):
+    pk = kwargs.get('pk')
+    obj = BookTitle.objects.get(pk=pk)
+    return render(request, 'books/detail.html', {'obj': obj})
