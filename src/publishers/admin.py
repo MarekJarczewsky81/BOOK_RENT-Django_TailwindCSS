e378c3083ae9bf1c27ab55b7aad936ed58038d21
Marek Jarczewski <marek.jarczewski81@gmail.com>
2023-10-18 12:11:06 +0200

    Added class PublisherResource and create a date in export data

--- src/publishers/admin.py
+++ src/publishers/admin.py
@@ -3,13 +3,19 @@ from .models import Publisher
 # import-export
 from import_export import resources
 from import_export.admin import ExportActionMixin
+from import_export.fields import Field
 
 # Register your models here.
 
 class PublisherResource(resources.ModelResource):
+    created = Field()
     class Meta:
         model = Publisher
         fields = ('id', 'name', 'country', 'created')
+        
+    def dehydrate_created(self, obj):
+        return obj.created.strftime("%d/%m/%y")
+        
     
     
     
